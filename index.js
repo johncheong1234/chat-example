602b61b157f88c68c8b3187addc30ad7f7dd3f7a
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-09-15 11:47:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  // console.log(req.params.user)
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  console.log(socket.handshake)
+
+  socket.join(socket.handshake.auth.sender_value);
+
+  socket.on('chat message', (msg)=>{
+
+     my_array = msg.split('_')
+
+      // io.emit('chat message', my_array[0]);
+      io.to(socket.handshake.auth.sender_value).to(socket.handshake.auth.receiver_value).emit('chat message', my_array[0]);
+
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
