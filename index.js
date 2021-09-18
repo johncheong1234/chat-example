745c501b916cceb300b02d0fe3f38ecb4395a5b2
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-09-18 21:34:01 +0800

    create msg to database

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const axios = require('axios')
+const crypto = require("crypto");
+
 app.use(express.urlencoded({
   extended: true
 }))
@@ -17,6 +19,21 @@ app.set('view engine', 'html');
 async function getData(url, stringData) {
   let resp = await axios({
     method: 'get',
+    url: url,
+    data: stringData,
+    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+    maxContentLength: 100000000,
+    maxBodyLength: 1000000000
+  }).catch(err => {
+    throw err;
+  })
+  console.log("getData: response:", resp.data);
+  return resp;
+}
+
+async function postData(url, stringData) {
+  let resp = await axios({
+    method: 'post',
     url: url,
     data: stringData,
     headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
@@ -54,11 +71,22 @@ io.on('connection', (socket) => {
 
   socket.join(socket.handshake.auth.username);
 
-  socket.on('chat message', ({msg, sender, receiver})=>{
+  socket.on('chat message', async ({msg, sender, receiver})=>{
     console.log('chat message sent')
     console.log(msg, sender, receiver)
-    io.to(socket.handshake.auth.username).to(receiver).emit('chat message', {msg, sender, receiver});
+    crypto.randomBytes(8, async(err, buf) => {
+      if (err) {
+        // Prints error
+        console.log(err);
+        return;
+      }
 
+      const randomid = buf.toString('hex')
+      io.to(socket.handshake.auth.username).to(receiver).emit('chat message', {msg, sender, receiver, randomid});
+      const chat_details = {"message":msg,"sender":sender,"receiver":receiver, "randomid": randomid}
+      postData("http://44.197.34.158:8083/api/messages",chat_details)
+    })
+    
   })
 
   socket.on('disconnect', () => {
